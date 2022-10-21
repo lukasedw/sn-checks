95c6605b6e550ff130f3677ee8503357cdbc625a
Lucas Eduardo <lukaszonecell@gmail.com>
2022-10-21 09:40:34 -0300

    Add another case overall spec

--- spec/requests/overall_spec.rb
+++ spec/requests/overall_spec.rb
@@ -2,7 +2,12 @@ require "rails_helper"
 
 RSpec.describe "Overalls", type: :request do
   describe "GET /index" do
-    it "returns http success", vcr: { cassette_name: "overall/success" } do
+    it "returns http success over social network request success", vcr: { cassette_name: "overall/success" } do
+      get "/"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http success over social network request failure", vcr: { cassette_name: "overall/failure" } do
       get "/"
       expect(response).to have_http_status(:success)
     end
